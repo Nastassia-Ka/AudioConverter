364b9fb069aba341d7645aa961fecd5470aac0a2
Alex <zarjrw1994@gmail.com>
2022-08-04 22:27:09 +0300

    Added descriptions AudioappConfig

--- AudioApp/apps.py
+++ AudioApp/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class AudioappConfig(AppConfig):
+    """Настройка приложения"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'AudioApp'
     verbose_name = 'Audio converter'
