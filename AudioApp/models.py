8d206c17de4e12296278b9661594bf8dcb6ea99f
Alex <zarjrw1994@gmail.com>
2022-08-04 22:26:10 +0300

    New database,added verbose name. Added settings model.

--- AudioApp/models.py
+++ AudioApp/models.py
@@ -10,18 +10,21 @@ class UserSong(models.Model):
         return str(self.audio_file) + str(self.title)
 
 
-class TrackData(models.Model):
+class AudioData(models.Model):
     """База данных для хранения данных о треках"""
-    login = models.CharField(max_length=100)
-    trek_name = models.CharField(max_length=100)
-    original_format = models.CharField(max_length=100)
-    original_tracks = models.CharField(max_length=100)
-    convertable_tracks = models.CharField(max_length=100)
-    convertable_format = models.CharField(max_length=100)
-    date = models.CharField(max_length=100)
+    login = models.CharField(max_length=100, verbose_name='Имя пользователя')
+    trek_name = models.CharField(max_length=100, verbose_name='Имя трека')
+    original_format = models.CharField(max_length=100, verbose_name='Формат оригинала')
+    original_track = models.CharField(max_length=100, verbose_name='Оригинальный трек')
+    convertable_track = models.CharField(max_length=100, verbose_name='Сконвертированный трек')
+    convertable_format = models.CharField(max_length=100, verbose_name='Формат сконвертированного трека')
+    date = models.CharField(max_length=100, verbose_name='Дата конвертации')
 
     def __str__(self):
-        return str(self.date)
+        return str(self.login)
 
-
-
+    class Meta:
+        """Настройки модели"""
+        verbose_name = 'Трек пользователя'
+        verbose_name_plural = 'Треки пользователя'
+        ordering = ['date']
