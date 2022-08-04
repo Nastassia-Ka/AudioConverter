351a757f7374a346b5dbd02bc484030459b91c19
Alex <zarjrw1994@gmail.com>
2022-08-04 22:28:48 +0300

    New name audio table, new field names.

--- AudioApp/utils/database.py
+++ AudioApp/utils/database.py
@@ -1,4 +1,4 @@
-from ..models import TrackData
+from ..models import AudioData
 
 def write_database(data_dict : dict) ->bool :
     """Сохраняет данные о треке в базу данных"""
@@ -6,14 +6,14 @@ def write_database(data_dict : dict) ->bool :
     login :str = data_dict.get('user_name', '')
     trek_name :str = data_dict.get('trek_name', '')
     original_format  :str = data_dict.get('original_format', '')
-    original_tracks :str = data_dict.get('path_original', '')
-    convertable_tracks :str = data_dict.get('path_convert', '')
+    original_track :str = data_dict.get('path_original', '')
+    convertable_track :str = data_dict.get('path_convert', '')
     convertable_format :str= data_dict.get('format', '')
     date :str = data_dict.get('date', '')
 
     try:
-        database :TrackData = TrackData(login=login, trek_name=trek_name, original_format=original_format,
-                          original_tracks=original_tracks, convertable_tracks=convertable_tracks,
+        database :AudioData = AudioData(login=login, trek_name=trek_name, original_format=original_format,
+                          original_track=original_track, convertable_track=convertable_track,
                           convertable_format=convertable_format, date=date)
         database.save()
     except:
