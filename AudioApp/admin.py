33552d26863fca441f90eedcb973a5160ca7856b
Alex <zarjrw1994@gmail.com>
2022-08-04 22:28:22 +0300

    New name audio table, new field names.

--- AudioApp/admin.py
+++ AudioApp/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import UserSong, TrackData
+from .models import UserSong, AudioData
+
+class AudioDataAdmin(admin.ModelAdmin):
+    """Класс настроек для отображения модели TrackData в админке"""
+    list_display = ('login', 'trek_name', 'original_format', 'date')
+    search_fields = ('login', 'date')
+    list_filter = ('login', 'original_format', 'convertable_format', 'date')
+
 
 admin.site.register(UserSong)
-admin.site.register(TrackData)
+admin.site.register(AudioData, AudioDataAdmin)
 
 # Register your models here.
