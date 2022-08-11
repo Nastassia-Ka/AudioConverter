3bede41f74c5ce78f4c3a54b43cb54b2f22c4d79
Alex <zarjrw1994@gmail.com>
2022-08-11 19:38:15 +0300

    Added user regestration page.

--- AudioApp/urls.py
+++ AudioApp/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.upload_file, name='home'),
     path('user/', views.user_account, name='user_account'),
+    path('login/',views.login, name='login'),
+    path('registration', views.user_reg, name='user_reg'),
     path('about/', views.about, name='about'),
     path('deletion/', views.deletion_page, name='deletion page'),
-    path('login/',views.login, name='login'),
 ]
